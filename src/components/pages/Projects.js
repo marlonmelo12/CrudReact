4ebf8be3d6974d93beca6930e61fd73c76cdd70b
Marlon Melo <133883966+marlonmelo12@users.noreply.github.com>
2024-06-12 18:44:11 -0300

    primeiro commit

---
+++ src/components/pages/Projects.js
@@ -0,0 +1,71 @@
+import { useLocation } from "react-router-dom"
+
+import Message from "../layout/Message"
+import styles from './Projects.module.css'
+import Container from '../layout/Container'
+import LinkButton from "../layout/LinkButton"
+import ProjectCard from "../project/ProjectCard"
+import { useState, useEffect } from "react"
+import Loading from "../layout/Loading"
+
+
+function Projects(){
+
+    const[projects, setProjects] = useState([])
+    const[removeLoading, setRemoveLoading] = useState(false)
+
+    const location = useLocation()
+    let message = ''
+    if(location.state){
+        message = location.state.message
+    }
+    useEffect(() => {
+
+        setTimeout(() => {
+            fetch('http://localhost:5000/projects', {
+                method: "GET",
+                headers:{
+                    'Content-Type' : 'aplication/json',
+                },
+            }).then(resp => resp.json())
+            .then(data => {
+                setProjects(data)
+                setRemoveLoading(true)
+                console.log(data)
+            })
+            .catch(err => console.log(err))
+        }, 300
+
+        )
+
+    },[])
+
+    return(
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto"/>
+            </div>
+            <Container customClass="start">
+                {projects.length >0 &&
+                projects.map((project) => (
+                    <ProjectCard 
+                    id={project.id}
+                    name={project.name}
+                    budget={project.budget}
+                    category={project.category.name}
+                    key={project.id}
+                    />
+                ))
+                }
+                {!removeLoading && <Loading/>}
+                {removeLoading && projects.length === 0 && (
+                    <p>Não a projetos cadastrados!</p>
+                )}
+            </Container>
+            {message && <Message type="success" msg={message} />}
+
+        </div>
+    )
+}
+export default Projects
